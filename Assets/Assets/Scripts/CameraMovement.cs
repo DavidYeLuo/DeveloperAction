8bcecda64a842465472e9da8c3d88ce96724db79
DavidYeLuo <dayeluo@galileoweb.org>
2018-08-16 21:36:18 -0700

    Deleted Unused Code
    CameraMovement class is now being used as a helper class.

--- Assets/Assets/Scripts/CameraMovement.cs
+++ Assets/Assets/Scripts/CameraMovement.cs
@@ -13,43 +13,6 @@ public class CameraMovement : MonoBehaviour
      */
     public float sensitivity;
 
-    void Update()
-    {
-        //Move();
-    }
-
-    private void Move()
-    {
-        /**
-        * Rotating the camera position.
-        * Rotates the camera based on the user's mouse movement
-        * Simulating a first person shooter
-        */
-
-        float horInput = Input.GetAxis("Mouse X") * sensitivity;
-        /**
-         * Problem to solve:
-         * Having the rotation just in the camera which is
-         * inside of the character makes movement awkward.
-         * Because the direction of foward is always static,
-         * pressing w and moving your mouse wouldn't affect
-         * the player movement.
-         * The player will have to have a sense of where the
-         * character is facing
-         * A more intuitive control
-         * TODO:
-         * Rotate by the y-axis from the character rather than the camera
-         */
-        float verInput = Input.GetAxis("Mouse Y") * sensitivity;
-
-        Vector3 targRot = transform.rotation.eulerAngles;
-
-        targRot.y += horInput;
-        targRot.x -= verInput;
-
-        transform.rotation = Quaternion.Euler(targRot);
-    }
-
     public void RotateAroundXaxis()
     {
         /**
